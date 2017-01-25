c77b8781a0ffef52663df739a34ba26ccb1fd452
aellis928 <mrskawasaki.90@gmail.com>
2017-01-24 22:17:21 -0800

    Working on the javascript part to add an active class to the links

---
+++ js/new.js
@@ -0,0 +1,15 @@
+// Getting the nav bar to higlight on the hover
+function highlight_navlink(linkname){
+
+  console.log(linkname)
+  linkname.className += " active"
+  console.log(linkname)
+
+}
+
+// so basically I think I know how to add an active class
+// I first need to remove the active class from all of the navlinks
+// then i need to add the "active" class to the nav link that was clicked
+// for some reason that background-color doesnt stay which is weird
+// need to figure out why it wont stay...
+// PROGRESS!! =)
